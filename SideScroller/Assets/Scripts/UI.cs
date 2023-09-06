3373109a03dd2d75cde93f84fd7b5c82297b86f9
AIE\s220465 <s220465@students.aie.edu.au>
2023-09-06 15:18:58 +0930

    New menu scene started

--- SideScroller/Assets/Scripts/UI.cs
+++ SideScroller/Assets/Scripts/UI.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField]
+    iTween.EaseType _easeType;
 
-
+    private Vector3 _position = new Vector3(0, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,21 @@ public class UI : MonoBehaviour
     void Update()
     {
        
+    }
+
+    void OnShopClick()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("position", transform.position + Vector3.left * 300f);
+        ht.Add("time", 0.5f);
+        ht.Add("easeType", _easeType);
+
+
+        iTween.MoveFrom(gameObject, ht);
     }
 }
