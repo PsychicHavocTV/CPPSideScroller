60d30c799f6577700ef0d418151b68513acb3738
Emmanuel Williams <emmanuel.letton@gmail.com>
2023-09-06 11:51:37 +0930

    ai framework (creatures)
    - Added beginning functionality for creature (SHARK)
    
    - Added Scriptable Objects creation for creatures.

---
+++ SideScroller/Assets/Scripts/CreatureObjects.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObstacleCreature", menuName = "Obstacles/ObstacleCreature", order = 1)]
+
+public class CreatureObjects : ScriptableObject
+{
+    public bool shark = false;
+    public bool seal = false;
+    public float creatureSpeed = 0;
+}
