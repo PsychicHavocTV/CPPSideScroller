03094a9a0df7bcb2f59541f4e58a5a0eb6558102
Emmanuel Williams <emmanuel.letton@gmail.com>
2023-09-13 16:25:29 +0930

    update

---
+++ SideScroller/Assets/Scripts/RunUIController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RunUIController : MonoBehaviour
+{
+    public PlayerController pC;
+
+    public TextMeshProUGUI distanceCounterText;
+    public TextMeshProUGUI coinCounterText;
+
+    int roundedDistance = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roundedDistance != (int)pC.runDistanceTravelled)
+        {
+            roundedDistance = (int)pC.runDistanceTravelled;
+        }
+
+        if (GameManager.Instance.gameOver == false)
+        {
+            distanceCounterText.text = roundedDistance.ToString() + " METERS";
+            coinCounterText.text = "$" + pC.currency.earntRunCurrency.ToString();
+        }
+    }
+}
